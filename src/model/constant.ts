1a7b93054b6176aa82d6ccd36141fbef02b90670
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-09-16 16:28:14 +0900

    🔧 chore: Add constants

---
+++ src/model/constant.ts
@@ -0,0 +1,7 @@
+export const TIME_ZONE = 9
+export const TZ_MSEC = TIME_ZONE * 60 * 60 * 1000
+
+export const MAX_EQK_INFO_LEN = 120
+export const MAX_EQK_STR_LEN = 60
+
+export const HEADER_LEN = 4
