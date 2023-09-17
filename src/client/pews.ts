584698423935221d3c8bce2bc4aca301e017f74b
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-09-17 15:40:13 +0900

    ✨ feat: Implement MMI parse

--- src/client/pews.ts
+++ src/client/pews.ts
@@ -1,18 +1,30 @@
-import { TZ_MSEC } from '../model/constant'
+import { HEADER_LEN, SYNC_PERIOD, TZ_MSEC } from '../model/constant'
 import { type Station } from '../types/model'
 import * as HTTP from './http'
 
 export class PEWS {
-  private readonly _phase = 1
   private readonly delay = 1000
 
+  private _phase = 1
   private staList: Station[] = []
-
   private tide: number = this.delay
   private needSync = true
 
+  private timeSyncIntervalID?: NodeJS.Timeout
+  private readonly mainLoopIntervalID?: NodeJS.Timeout
+
   constructor () {
     this.tide = this.delay
+  }
+
+  get phase (): number {
+    return this._phase
+  }
+
+  private startTimeSyncInterval (): void {
+    this.timeSyncIntervalID = setInterval(() => {
+      this.needSync = true
+    }, SYNC_PERIOD * 1000)
   }
 
   private getTimeString (): string {
@@ -36,7 +48,20 @@ export class PEWS {
     this.needSync = false
   }
 
-  private staBinStrHandler (bianryStr: string): void {
+  async getSta (): Promise<any> {
+    const res = await HTTP.getSta(this.getTimeString())
+    const byteArray = res.data
+
+    let binaryStr = ''
+
+    for (const i of byteArray) {
+      binaryStr += i.toString(2).padStart(8, '0')
+    }
+
+    await this.staBinStrHandler(binaryStr)
+  }
+
+  private async staBinStrHandler (bianryStr: string): Promise<void> {
     const newStaList: Station[] = []
     const staLatArr: number[] = []
     const staLonArr: number[] = []
@@ -55,26 +80,75 @@ export class PEWS {
     }
   }
 
-  async getSta (): Promise<any> {
-    const res = await HTTP.getSta(this.getTimeString())
-    const byteArray = new Uint8Array(res.data)
+  async getMMI (): Promise<any> {
+    const res = await HTTP.getMMI(this.getTimeString())
+    const byteArray = res.data
 
+    let header = ''
     let binaryStr = ''
 
-    for (const i of byteArray) {
-      binaryStr += i.toString(2).padStart(8, '0')
+    for (let i = 0; i < HEADER_LEN; i++) {
+      header += byteArray[i].toString(2).padStart(8, '0')
     }
 
-    this.staBinStrHandler(binaryStr)
-    console.log(this.staList)
+    for (let i = HEADER_LEN; i < byteArray.length; i++) {
+      binaryStr += byteArray[i].toString(2).padStart(8, '0')
+    }
+
+    const staF = header.slice(0, 1) === '1'
+
+    if (header.slice(1, 2) === '0' && header.slice(2, 3) === '0') {
+      this._phase = 1
+    } else if (header.slice(1, 2) === '1' && header.slice(2, 3) === '0') {
+      this._phase = 2
+    } else if (header.slice(1, 2) === '0' && header.slice(2, 3) === '1') {
+      this._phase = 3
+    } else if (header.slice(1, 2) === '1' && header.slice(2, 3) === '1') {
+      this._phase = 4
+    }
+    if (staF || this.staList.length < 99) {
+      await this.getSta()
+    }
+    await this.mmiBinStrHandler(binaryStr)
   }
 
-  get phase (): number {
-    return this._phase
+  private async mmiBinStrHandler (binaryStr: string): Promise<void> {
+    const mmiArr: number[] = []
+
+    for (let i = 0; i < binaryStr.length; i += 4) {
+      mmiArr.push(parseInt(binaryStr.slice(i, i + 4), 2))
+    }
+
+    for (let i = 0; i < this.staList.length; i++) {
+      this.staList[i].mmi = mmiArr[i]
+    }
+  }
+
+  async loop (): Promise<void> {
+    while (true) {
+      if (this.needSync) {
+        await this.syncTime()
+      }
+
+      await this.getMMI()
+
+      await new Promise(resolve => setTimeout(resolve, 1000))
+    }
   }
 
   async run (): Promise<void> {
-    await this.syncTime()
+    if (this.needSync) {
+      this.startTimeSyncInterval()
+    }
+
     await this.getSta()
+    await this.getMMI()
+
+    await this.loop()
+  }
+
+  stop (): void {
+    clearInterval(this.timeSyncIntervalID)
+    clearInterval(this.mainLoopIntervalID)
   }
 }
