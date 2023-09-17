9b928302655135fc46c595e5ef6d9696446cf5d6
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-09-17 15:39:57 +0900

    🥅 chore: Add Error handler

--- src/client/http.ts
+++ src/client/http.ts
@@ -1,18 +1,38 @@
-import axios, { type AxiosResponse } from 'axios'
+import axios, { type AxiosResponse, AxiosError } from 'axios'
+import { HTTPError } from '../utils/error'
 
 const client = axios.create({
   baseURL: 'https://www.weather.go.kr/pews/',
   timeout: 1000
 })
 
+const handleError = (err: unknown, event: string): unknown => {
+  if (err instanceof AxiosError && err.response !== undefined) {
+    return new HTTPError(err.message, err.response.status, event)
+  }
+  return err
+}
+
 export const get = async (url?: string): Promise<AxiosResponse> => {
-  return await client.get(url ?? '')
+  try {
+    return await client.get(url ?? '')
+  } catch (err) {
+    throw handleError(err, 'get')
+  }
 }
 
-export const getMMI = async (url: string): Promise < AxiosResponse > => {
-  return await client.get(`${url}.b`)
+export const getMMI = async (url: string): Promise<AxiosResponse<Uint8Array>> => {
+  try {
+    return await client.get(`data/${url}.m`, { responseType: 'arraybuffer' })
+  } catch (err) {
+    throw handleError(err, 'getMMI')
+  }
 }
 
-export const getSta = async (url: string): Promise < AxiosResponse< ArrayBuffer >> => {
-  return await client.get(`data/${url}.s`, { responseType: 'arraybuffer' })
+export const getSta = async (url: string): Promise<AxiosResponse<Uint8Array>> => {
+  try {
+    return await client.get(`data/${url}.s`, { responseType: 'arraybuffer' })
+  } catch (err) {
+    throw handleError(err, 'getSta')
+  }
 }
