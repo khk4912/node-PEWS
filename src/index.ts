1695475e9392cd503de3c30ce481173b435faa30
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-01 02:30:11 +0900

    ⚰️ chore: Remove comments in index

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-// import { event } from './client/event_deco'
-// import { PEWS } from './client/pews'
-
 export { PEWS as default } from './client/pews'
-export { PEWS } from './client/pews'
 export { SimulationData } from './sim/sim_data'
 
 export { LoggingLevel, Logger } from './utils/logger'
 export { HTTPError } from './utils/error'
-
-// class TEST extends PEWS {
-//   @event('loop')
-//   newEEW (data: any): void {
-//     console.log('asdf')
-//   }
-// }
-
-// const test = new TEST()
-
-// test.start()
