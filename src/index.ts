21d26750e40901f755eaa69d01f41f88b00018e8
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-07 15:50:30 +0900

    ✨ feat: Add SimulationParams for user using deco

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { PEWS as default } from './client/pews'
-export { SimulationData } from './sim/sim_data'
+export { event } from './client/event_deco'
+export { SimulationData, SimulationParams } from './sim/sim_data'
 
 export { type LoggingLevel, Logger } from './utils/logger'
 export { HTTPError } from './utils/error'
--- src/sim/sim_data.ts
+++ src/sim/sim_data.ts
@@ -5,3 +5,9 @@ export const SimulationData = {
   POHANG: new PEWS(true, 2017000407, new Date(2017, 10, 15, 14, 29, 21), new Date(2017, 10, 15, 14, 34, 20)),
   GYEONGJU: new PEWS(true, 2016000291, new Date(2016, 8, 12, 20, 32, 44), new Date(2016, 8, 12, 20, 37, 43))
 }
+
+export const SimulationParams = {
+  JEJU: [true, 2021007178, new Date(2021, 11, 14, 17, 19, 9), new Date(2021, 11, 14, 17, 27, 23)],
+  POHANG: [true, 2017000407, new Date(2017, 10, 15, 14, 29, 21), new Date(2017, 10, 15, 14, 34, 20)],
+  GYEONGJU: [true, 2016000291, new Date(2016, 8, 12, 20, 32, 44), new Date(2016, 8, 12, 20, 37, 43)]
+}
