304d29238408f26283903556d29cee4f62bd5f0c
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-08 17:50:03 +0900

    ♻️ refactor: No more default export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export { PEWS as default } from './client/pews'
-export { event } from './client/event_deco'
+export { PEWS } from './client/pews'
 export { SimulationData, SimulationParams } from './sim/sim_data'
 
 export { type LoggingLevel, Logger } from './utils/logger'
 export { HTTPError } from './utils/error'
+
+export { event } from './client/event_deco'
