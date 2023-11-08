cdba54f39e74425319209e516eb92782f14e39d4
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-08 18:05:18 +0900

    ✨ feat: Now export EEWInfo, EqkInfo

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ export { SimulationData, SimulationParams } from './sim/sim_data'
 export { type LoggingLevel, Logger } from './utils/logger'
 export { HTTPError } from './utils/error'
 
+export { EEWInfo, EqkInfo } from './model/eqk_model'
+
 export { event } from './client/event_deco'
