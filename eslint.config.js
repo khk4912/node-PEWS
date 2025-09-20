0885280afa0f0cdaa83da4316450c7110655acf1
khk4912 <30457148+khk4912@users.noreply.github.com>
2025-09-20 17:05:51 +0900

    ⬆️ chore: Upgrade dependencies & Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,30 +2,21 @@ import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import neostandard from 'neostandard'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default tseslint.config(
+export default defineConfig(
+  globalIgnores(['dist', '**/runtime_test/**', '**/pews.js']),
   ...neostandard({
-    ignores: ['dist', 'runtime_test', '**/*.js'],
+    ts: true,
     filesTs: ['**/*.{ts,tsx}'],
-    ts: true
-  }),
-  { ignores: ['dist', 'runtime_test', '**/*.js'] },
-  {
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked
+      tseslint.configs.recommendedTypeChecked,
     ],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: import.meta.dirname
-      },
       ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    }
-  }
+      globals: globals.browser,
+    },
+  })
 )
