3cc72a3ac58ef9340232ff766be18c9ca5cf5a89
khk4912 <30457148+khk4912@users.noreply.github.com>
2025-09-20 17:57:51 +0900

    🔧 chore: Update Rollup configuration to support .d.ts

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,11 +1,46 @@
 import { defineConfig } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
 
-export default defineConfig({
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'esm'
+const tsPlugin = typescript({
+  tsconfig: './tsconfig.json',
+  compilerOptions: {
+    declaration: true,
+    declarationDir: 'dist/types',
+    rootDir: 'src',
+    noEmit: false
   },
-  plugins: [typescript()]
+  outputToFilesystem: true
 })
+
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'esm',
+        sourcemap: true
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      }
+    ],
+    plugins: [tsPlugin]
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es'
+    },
+    plugins: [dts()],
+    onwarn (warning, warn) {
+      if (warning.code === 'EMPTY_BUNDLE') return
+      warn(warning)
+    }
+  }
+])
